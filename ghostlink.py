a59d6d8fabdcabafa77b4c4e29fac9f0b6417d8d
d0t <d0t@d0t>
2025-07-16 22:20:40 +0200

    Update source code and license

--- ghostlink.py
+++ ghostlink.py
@@ -1,22 +1,38 @@
-# gh0stl1nk.py — WAPA ghostlink
-# Copyright (C) 2025 D0t
+# BSD 3-Clause License
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Copyright (c) 2025, Diego (0xD0t).
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import time
 import base64
 import logging
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
 import os, sys
 import threading
 from hashlib import sha256
@@ -30,9 +46,6 @@ from protocol import fragment_file, decrypt_fragment, parse_decrypted
 # ==============================
 #       General config
 # ==============================
-iface = "mon1"                      #| default
-username = "d0t"                    #| default
-cipher_key = b"mysharedsecret00"    #| default (16 caracteres)
 count = 5
 maxpayload = 1024
 verbose = False
@@ -51,18 +64,30 @@ current_input = ""
 #            Utils
 # ==============================
 def pretty_printer(user, msg):
+    """
+    Prettify code output with special characters
+    """
     sys.stdout.write("\r" + " " * (len(input_request_message) + len(current_input)) + "\r")
     print(f"[{user}]: {msg}")
     sys.stdout.write(input_request_message + current_input)
     sys.stdout.flush()
 
 def current_timestamp():
+    """
+    Get the current time
+    """
     return int((time.time() - bootime) * 1000000)
 
 def checksum(data):
+    """
+    Calculate checksum of messages and return last 5 characters
+    """
     return sha256(data).hexdigest()[:8]
 
 def chatencrypt(message):
+    """
+    Cipher message and encode it with base 64
+    """
     message = username + "~" + message
     if len(message) < maxpayload:
         message = message.rjust(maxpayload)
@@ -76,6 +101,9 @@ def chatencrypt(message):
     return base64.b64encode(iv + ciphertext)
 
 def decrypt_payload(data):
+    """
+    Decrypt message and return (user, content)
+    """
     try:
         raw = base64.b64decode(data)
         iv, ciphertext = raw[:16], raw[16:]
@@ -88,6 +116,9 @@ def decrypt_payload(data):
     return None, None
 
 def build_packet(encrypted_msg):
+    """
+    Create 802.11 packet with a payload
+    """
     dot11 = Dot11(type=0, subtype=13, addr1="ff:ff:ff:ff:ff:ff", addr2=RandMAC(), addr3="ff:ff:ff:ff:ff:ff")
     pkt = RadioTap()/dot11/Raw(load=encrypted_msg)
     pkt.timestamp = current_timestamp()
@@ -95,9 +126,9 @@ def build_packet(encrypted_msg):
 
 def wait_for_ack(msg_hash, timeout=1.0, interval=0.05):
     """
-    Wait for ACKs.
-    Checks again every 'interval' seconds.
-    If ACK, True, else False.
+    Wait for ACKs
+    Checks again every 'interval' seconds
+    If ACK, True, else False
     """
     deadline = time.time() + timeout
     while time.time() < deadline:
@@ -118,7 +149,7 @@ def send_plain(msg):
 
 def announce_user(user, status):
     """
-    Announce users in the room (either new users or users that left the channel)
+    Announce users in the room/channel (either new users or users that left the channel)
     """
     if status == "join":
         encrypted_ann = chatencrypt("[USR-ANN]:" + str(user))
@@ -145,19 +176,16 @@ def send_encrypted_msg(msg):
     pkt       = build_packet(encrypted)
     msg_hash  = checksum(encrypted)
 
-    # ACK waitlist
     with ack_lock:
         ack_wait[msg_hash] = time.time()
     if verbose:
         print(f"[ghostlink] awaiting ACK for {msg_hash}")
 
-    # Transmissions + await for ACK
     for attempt in range(count):
         sendp(pkt, iface=iface, verbose=0)
         if verbose:
             print(f"[ghostlink] sent {msg_hash}, attempt {attempt+1}/{count}")
 
-        # ACK waiting space
         if wait_for_ack(msg_hash, timeout=1.0):
             if verbose:
                 print(f"[ACK] received for {msg_hash}")
@@ -167,6 +195,9 @@ def send_encrypted_msg(msg):
 
 
 def handle_ack(msg):
+    """
+    Manage received ACKs
+    """
     if msg.startswith("[RCV-ACK]"):
         ack_hash = msg.split(":", 1)[1].strip()
         with ack_lock:
@@ -176,18 +207,27 @@ def handle_ack(msg):
                 del ack_wait[ack_hash]
 
 def user_joined(msg):
+    """
+    Acts when a user joins the room
+    """
     sys.stdout.write("\r" + " " * (len(input_request_message) + len(current_input)) + "\r")
     print(f"[!] User {msg.split(':', 1)[1].strip()} just joined the room.")
     sys.stdout.write(input_request_message + current_input)
     sys.stdout.flush()
 
 def user_left(msg):
+    """
+    Acts when a user leaves the room
+    """
     sys.stdout.write("\r" + " " * (len(input_request_message) + len(current_input)) + "\r")
     print(f"[!] User {msg.split(':', 1)[1].strip()} left the room.")
     sys.stdout.write(input_request_message + current_input)
     sys.stdout.flush()
 
 def is_duplicate(payload):
+    """
+    Check if the message is duplicated
+    """
     digest = sha256(payload).digest()
     if digest in recent_messages:
         return True
@@ -199,28 +239,37 @@ def is_duplicate(payload):
     return False
 
 def send_file(path):
+    """
+    Fragments a source file and sends each fragment {count} times
+    """
     if not os.path.isfile(path):
-        print(f"[!] Archivo no encontrado: {path}")
+        print(f"[!] File not found: {path}")
         return
     fragments = fragment_file(path)
-    print(f"[>] Enviando {len(fragments)} fragmentos desde: {path}")
+    print(f"[>] Sending {len(fragments)} fragments from: {path}")
     for i, frag in enumerate(fragments, 1):
         pkt = build_packet(frag)
         sendp(pkt, iface=iface, count=count, verbose=0)
-        print(f"  - Fragmento {i}/{len(fragments)} enviado")
+        print(f"  - Fragment {i}/{len(fragments)} sent")
 
 # ==============================
 #      Package Reception
 # ==============================
 def save_file(session_id, fragments_dict):
+    """
+    Orders received fragments and creates the source file in the destination system
+    """
     ordered = [fragments_dict[i] for i in sorted(fragments_dict.keys())]
     output = b"".join(ordered)
     filename = f"ghostlink_recv_{session_id}.bin"
     with open(filename, "wb") as f:
         f.write(output)
-    print(f"[✔] Archivo recibido y guardado como: {filename}")
+    print(f"[+] File received and saved: {filename}")
 
 def is_fragmented_file(payload):
+    """
+    Checks if the received frame's payload is a fragmented file (or a message)
+    """
     try:
         decrypted = decrypt_fragment(payload)
         parts = decrypted.split(b"|", 2)
@@ -236,6 +285,9 @@ def is_fragmented_file(payload):
         return False
 
 def handle_fragment(payload_b64):
+    """
+    Manage file fragments
+    """
     global file_sessions
     try:
         decrypted = decrypt_fragment(payload_b64)
@@ -257,6 +309,9 @@ def handle_fragment(payload_b64):
         print(f"[!] Error al procesar fragmento: {e}")
 
 def packet_handler(pkt):
+    """
+    Scapy packet handler
+    """
     global username
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 13 and pkt.haslayer(Raw):
         raw_data = pkt[Raw].load
@@ -276,7 +331,7 @@ def packet_handler(pkt):
                 elif msg.startswith("[USR-LFT]"):
                     user_left(msg)
                 else:
-                    #print(f"\n[{user}]: {msg}")                 
+                    #print(f"\n[{user}]: {msg}")
                     pretty_printer(user, msg)                   # print received packet
                     ack = f"[RCV-ACK]: {checksum(raw_data)}"    # calculate ACK checksum
                     send_plain(ack)                             # and send it back
@@ -285,14 +340,23 @@ def packet_handler(pkt):
 # MAIN
 # ==============================
 def start_sniffer():
+    """
+    Starts the Scapy sniffer
+    """
     sniff(iface=iface, prn=packet_handler, store=False)
 
 def adjust_psk(psk):
+    """
+    Adjust the cipher_key generated from the room name
+    """
     encoded_psk = psk.encode() 
     cipher_key = pad(encoded_psk, AES.block_size)
     return cipher_key[:16]
 
 def input_loop():
+    """
+    Waits for input and decides what to do with it
+    """
     global current_input
     while True:
         try:
@@ -312,7 +376,7 @@ def input_loop():
             break
 
 if __name__ == "__main__":
-    greeting = "        [*] Started WAPA gh0stl1nk        "
+    greeting = f"{' '*8}[*] Started WAPA gh0stl1nk{' '*8}"
     print(greeting)
     print("=" * len(greeting))
     iface = input("[>] Select the interface to use: ")
